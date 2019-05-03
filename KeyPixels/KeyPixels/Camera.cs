4b6834d6dd754089e6006b9bc1fd7338c6db149e
Vikram Apilla <vikramapilla@gmail.com>
2019-05-03 11:47:10 +0200

    added project

---
+++ KeyPixels/KeyPixels/Camera.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+
+namespace KeyPixels
+{
+    class Camera
+    {
+        public Vector3 position;
+        public Vector3 target;
+
+        public float fieldOfView;
+        public float aspectRatio;
+        public float nearPlane;
+        public float farPlane;
+
+        public Camera(GraphicsDeviceManager graphics)
+        {
+            position = new Vector3(2f, 0f, -2f);
+            target = Vector3.Zero;
+            fieldOfView = MathHelper.PiOver4;
+            aspectRatio = graphics.PreferredBackBufferWidth / graphics.PreferredBackBufferHeight;
+            nearPlane = 2f;
+            farPlane = 100f;
+        }
+
+    }
+}
---
+++ KeyPixels/KeyPixels/Game1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace KeyPixels
+{
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        Camera camera;
+        SpriteBatch spriteBatch;
+
+        Matrix worldMatrix;
+        Matrix viewMatrix;
+        Matrix projectionMatrix;
+
+        Model playerModel;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            camera = new Camera(graphics);
+        }
+
+
+        protected override void Initialize()
+        {
+            worldMatrix = Matrix.CreateTranslation(Vector3.Zero) * Matrix.CreateRotationZ(MathHelper.PiOver2);
+            viewMatrix = Matrix.CreateLookAt(camera.position, camera.target, Vector3.Up);
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(camera.fieldOfView, camera.aspectRatio, camera.nearPlane, camera.farPlane);
+
+            base.Initialize();
+        }
+        
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            playerModel = Content.Load<Model>("Models/Shot_Tria");
+        }
+        
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.SlateGray);
+
+            // TODO: Add your drawing code here
+            Draw3DModel(playerModel, worldMatrix, viewMatrix, projectionMatrix);
+            base.Draw(gameTime);
+        }
+
+        public static void Draw3DModel(Model model, Matrix worldMatrix, Matrix viewMatrix, Matrix projectionMatrix)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = worldMatrix;
+                    effect.View = viewMatrix;
+                    effect.Projection = projectionMatrix;
+                }
+                mesh.Draw();
+            }
+        }
+
+    }
+}
