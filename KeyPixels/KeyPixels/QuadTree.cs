a3fb1e26df1faf7e7e946833a3122d57074cc0a4
Martin Haase <m.haase@live.de>
2019-05-17 16:39:56 +0200

    Added QuadTree better seekData

--- KeyPixels/KeyPixels/QuadTree.cs
+++ KeyPixels/KeyPixels/QuadTree.cs
@@ -17,6 +17,8 @@ namespace KeyPixels
         private Node root;
         private float minDelta;
         private static bool insert;
+        private static float deltaX, deltaY;
+        private static List<Vector2> _list;
 
         public QuadTree(Vector2 _min, Vector2 _max, float _minDelta)
         {
@@ -30,43 +32,73 @@ namespace KeyPixels
 
         public List<T> seekData(Vector2 _min, Vector2 _max)
         {
-            List<T> ret_temp = new List<T>();
-            Stack<Vector2> _stack = new Stack<Vector2>();
-            _stack.Push(_min);
-            _stack.Push(_max);
-            _stack.Push(new Vector2(_min.X, _max.Y));
-            _stack.Push(new Vector2(_max.X, _min.Y));
+            Vector2 _vek = _min;
+            _list = new List<Vector2>();
 
-            while (_stack.Count > 0)
-            {
-                List<T> temp = _seekData(root, _stack.Pop());
-                bool seek = true;
-                for (int i = 0; i < temp.Count; ++i)
-                {
-                    for (int x = 0; x < ret_temp.Count && seek; ++x)
-                    {
-                        if (ret_temp[x].ToString() == temp[i].ToString())
-                            seek = false;
-                    }
-                    if (seek)
-                        ret_temp.Add(temp[i]);
-                    seek = true;
-                }
-            }
-            return ret_temp;
+            deltaX = MathHelper.Distance(root.min.X, root.max.X);
+            deltaY = MathHelper.Distance(root.min.Y, root.max.Y);
+            return _seekData(root, ref _vek, _max);
         }
 
-        private List<T> _seekData(Node temp, Vector2 vec)
+        private List<T> _seekData(Node temp, ref Vector2 vec, Vector2 _max)
         {
+            bool[] b_help = new bool[4];
+            for (int i = 0; i < 4; ++i)
+                b_help[i] = false;
+
             List<T> ret_temp = new List<T>();
             for (int i = 0; i < temp.child.Count; ++i)
             {
                 if (temp.child[i].min.X < vec.X && temp.child[i].min.Y < vec.Y &&
-                    temp.child[i].max.X > vec.X && temp.child[i].max.Y > vec.Y)
-                        ret_temp.AddRange(_seekData(temp.child[i], vec));
-                
+                    temp.child[i].max.X > vec.X && temp.child[i].max.Y > vec.Y && !b_help[i])
+                {
+                    deltaX = MathHelper.Distance(temp.child[i].min.X, temp.child[i].max.X);
+                    deltaY = MathHelper.Distance(temp.child[i].min.Y, temp.child[i].max.Y);
+                    ret_temp.AddRange(_seekData(temp.child[i], ref vec, _max));
+                    b_help[i] = true;
+                    i = -1;
+                }
             }
-            ret_temp.AddRange(temp.data);
+            float midlX = temp.min.X + (MathHelper.Distance(temp.min.X, temp.max.X) / 2),
+                midlY = temp.min.Y + (MathHelper.Distance(temp.min.Y, temp.max.Y) / 2);
+            if ((vec.X + deltaX > midlX && vec.X - deltaX < midlX) || (vec.Y + deltaY > midlY && vec.Y - deltaY < midlY))
+            {
+                ret_temp.AddRange(temp.data);
+                bool b1 = vec.X + midlX * 2 > temp.max.X && vec.X + midlX * 2 < _max.X;
+                bool b2 = vec.Y + midlX * 2 > temp.max.Y && vec.Y + midlY * 2 < _max.Y;
+                if (b1)
+                {
+                    bool seek = true;
+                    for (int s = 0; s < _list.Count; ++s)
+                        if (_list[s].X == vec.X + midlX * 2 && _list[s].Y == vec.Y)
+                            seek = false;
+                    if (seek)
+                        _list.Add(new Vector2(vec.X + midlX * 2, vec.Y));
+                }
+                if (b2)
+                {
+                    bool seek = true;
+                    for (int s = 0; s < _list.Count; ++s)
+                        if (_list[s].X == vec.X && _list[s].Y == vec.Y + midlY * 2)
+                            seek = false;
+                    if (seek)
+                        _list.Add(new Vector2(vec.X, vec.Y + midlY * 2));
+                }
+                if (b1 && b2)
+                {
+                    bool seek = true;
+                    for (int s = 0; s < _list.Count; ++s)
+                        if (_list[s].X == vec.X + midlX * 2 && _list[s].Y == vec.Y + midlY * 2)
+                            seek = false;
+                    if (seek)
+                        _list.Add(new Vector2(vec.X + midlX * 2, vec.Y + midlY * 2));
+                }
+            }
+            if (_list.Count > 0)
+            {
+                vec = _list[0];
+                _list.Remove(vec);
+            }
             return ret_temp;
         }
 
@@ -113,14 +145,14 @@ namespace KeyPixels
                     if (i < 4)
                     {
                         temp.child.Add(new Node());
-                        float minX = temp.min.X+ (MathHelper.Distance(temp.min.X, temp.max.X)/2);
+                        float minX = temp.min.X + (MathHelper.Distance(temp.min.X, temp.max.X) / 2);
                         float maxX = temp.max.X;
                         if (minX > _max.X)
                         {
                             maxX = minX;
                             minX = temp.min.X;
                         }
-                        float minY = temp.min.Y + (MathHelper.Distance(temp.min.Y, temp.max.Y)/2);
+                        float minY = temp.min.Y + (MathHelper.Distance(temp.min.Y, temp.max.Y) / 2);
                         float maxY = temp.max.Y;
                         if (minY > _max.Y)
                         {
